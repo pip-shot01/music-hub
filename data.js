a2681c20fe38fd068e21003d7f172085d8150458
nwamama8148 <davidjosiah44@gmail.com>
2022-08-24 03:11:30 -0700

    New

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: '/musics/Song_1.mp3',
+        artist: 'artist 1',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: '/musics/Song_2.mp3',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: '/musics/Song_3.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: '/musics/Song_4.mp3',
+        artist: 'artist 4',
+        cover: '/images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: '/musics/Song_5.mp3',
+        artist: 'artist 5',
+        cover: '/images/cover 5.png'
+    },
+    {
+        name: 'song 6',
+        path: '/musics/Song_6.mp3',
+        artist: 'artist 6',
+        cover: '/images/cover 6.png'
+    },
+    {
+        name: 'song 7',
+        path: '/musics/Song_7.mp3',
+        artist: 'artist 7',
+        cover: '/images/cover 7.png'
+    },
+    {
+        name: 'song 8',
+        path: '/musics/Song_8.mp3',
+        artist: 'artist 8',
+        cover: '/images/img-16.png'
+    },
+]
